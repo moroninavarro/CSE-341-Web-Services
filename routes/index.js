8a21743a8ddb6b2ad682c5106ab402bc74a78716
moroninavarro <moronialanis23@gmail.com>
2025-10-31 13:54:52 -0700

    change

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => { res.send('Hello World');});
 
+router.use('/Contacts', require('./Contacts'));
+
 module.exports = router;
